b57923be6b2b9a81cc88aa1f9e8a0d83e2a474a4
neyder.daza <mimr0694@gmail.com>
2017-10-07 13:33:19 -0500

    crear archivo excel

---
+++ example/app.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+    nodeExcel = require('excel-export'),
+    app = express();
+
+app.get('/Excel', function(req, res){
+    var conf ={};
+
+    conf.cols = [{
+        caption:'Grupo Mares',
+        captionStyleIndex: 1,
+        type:'string',
+        width:30
+      },{
+        caption:'Nombre Proyecto',
+        type:'string',
+        width:30
+    },{
+      caption:'Estudiantes',
+      type:'string',
+      width:30
+    },{
+      caption:'Correo',
+      type:'string',
+      width:30
+    },{
+      caption:'tutor',
+      type:'string',
+      width:30
+    },{
+      caption:'Correo',
+      type:'string',
+      width:30
+    },{
+      caption:'Estado',
+      type:'string',
+      width:30
+    },{
+      caption:'Observación',
+      type:'string',
+      width:30
+    },{
+      caption:'fecha de Aprobación',
+      type:'string',
+      width:30
+    }
+  ];
+
+    conf.rows = [
+      ['123-1', 'Ipisis 1', ["Neyder","Maria"], ["correo1","correo2"], ["tutor1","tutor2"], ["correo1","correo2"],"acepta","...","2017/08/06"],
+      ['123-2','Ipisis 2', ["Neyder","Maria"],  ["correo1","correo2"], ["tutor1","tutor2"], ["correo1","correo2"],"acepta","...","2017/08/06"],
+      ['123-3', 'Ipisis 3', ["Neyder","Maria"], ["correo1","correo2"], ["tutor1","tutor2"], ["correo1","correo2"],"acepta","...","2017/08/06"],
+      ['123-4', 'Ipisis 4', ["Neyder","Maria"], ["correo1","correo2"], ["tutor1","tutor2"], ["correo1","correo2"],"acepta","...","2017/08/06"]
+    ];
+  var result = nodeExcel.execute(conf);
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+  res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+  res.end(result, 'binary');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
